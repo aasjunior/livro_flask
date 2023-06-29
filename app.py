772f9bb2c696224255b29438bd1cb54ea59f87dc
aasjunior <adeldivo@hotmail.com>
2023-06-29 00:58:04 -0300

    feat: implementanto migrate e SQLALCHEMY

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 from config import app_config, app_active
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from model.models import *
 
 config = app_config[app_active]
-db = SQLAlchemy()
 
 def create_app(config_name):
     app = Flask(__name__, template_folder='template')
--- migrate.py
+++ migrate.py
@@ -15,41 +15,8 @@ migrate = Migrate(app, db)
 
 cli = FlaskGroup(create_app=create_app)
 
-class Role(db.Model):
-    id=db.Column(db.Integer, primary_key=True)
-    name=db.Column(db.String(40), unique=True, nullable=False)
-
-class User(db.Model):
-    id=db.Column(db.Integer, primary_key=True)
-    username=db.Column(db.String(40), unique=True, nullable=False)
-    email=db.Column(db.String(120), unique=True, nullable=False)
-    password=db.Column(db.String(80), nullable=False)
-    date_created=db.Column(db.DateTime(6), default=db.func.current_timestamp(), nullable=False)
-    last_update=db.Column(db.DateTime(6), onupdate=db.func.current_timestamp(), nullable=True)
-    recovery_code=db.Column(db.String(200), nullable=True)
-    active=db.Column(db.Boolean(), default=1, nullable=True)
-    role=db.Column(db.Integer, db.ForeignKey(Role.id), nullable=False)
-
-class Category(db.Model):
-    id=db.Column(db.Integer, primary_key=True)
-    name=db.Column(db.String(20), unique=True, nullable=False)
-    description=db.Column(db.Text(), nullable=False)
-
-class Product(db.Model):
-    id=db.Column(db.Integer, primary_key=True)
-    name=db.Column(db.String(20), unique=True, nullable=False)
-    description=db.Column(db.Text(), nullable=False)
-    qtd=db.Column(db.Integer, nullable=True, default=0)
-    image=db.Column(db.Text(), nullable=True)
-    price=db.Column(db.Numeric(10,2), nullable=False)
-    date_created=db.Column(db.DateTime(6), default=db.func.current_timestamp(), nullable=False) 
-    last_update=db.Column(db.DateTime(6), onupdate=db.func.current_timestamp(), nullable=False)
-    status=db.Column(db.Boolean(), default=1, nullable=True)
-    user_created=db.Column(db.Integer, db.ForeignKey(User.id), nullable=False)
-    category=db.Column(db.Integer, db.ForeignKey(Category.id), nullable=False)
-
 if __name__ == '__main__':
     cli()
 
     ## $env:FLASK_APP = "app:create_app('development')"
-    ## flask db init
+    ## flask db init, flask db migrate, flask db upgrade
